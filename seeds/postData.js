248911920abf3c14f38a16ebe1bf1d94537aa35d
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-25 17:47:14 -0500

    corrected post username

--- seeds/postData.js
+++ seeds/postData.js
@@ -4,12 +4,12 @@ const postData =
     {
       "title": "BobRoxx",
       "post_body": "Bob is the coolest no denying",
-      "user_id": "1",
+      "user_id": "2",
     },
     {
         "title": "Steve is great",
         "post_body": "Steve is way better",
-        "user_id": "2",
+        "user_id": "5",
     },
     {
         "title": "Hack the Planet",
