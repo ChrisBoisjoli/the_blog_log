0780208809e52ab428b52f45b66949df5524971c
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-25 06:46:35 -0500

    created Post model

---
+++ models/Post.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
+  Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      post_body: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_id: {
+          type: DataTypes.INTEGER,
+          references: {
+              model: 'user',
+              key: 'id'
+          }
+      },
+
+    },
+    {
+      hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post',
+    }
+  );
+  
+  module.exports = Post;
