0d74be281000341e3006e09b594eacae836431e8
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-26 13:23:39 -0500

    building dashboard

--- models/Post.js
+++ models/Post.js
@@ -40,7 +40,7 @@ class Post extends Model {
         },
       },
       sequelize,
-      timestamps: false,
+      timestamps: true,
       freezeTableName: true,
       underscored: true,
       modelName: 'post',
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,19 +1,10 @@
 module.exports = {
 
-
+  // format_time: (date) => {
+  //   return date.toLocaleTimeString();
+  // },
 
     
-    // get_emoji: () => {
-    //   const randomNum = Math.random();
-    //   let book = "📗";
-  
-    //   if (randomNum > 0.7) {
-    //     book = "📘";
-    //   } else if (randomNum > 0.4) {
-    //     book = "📙";
-    //   }
-  
-    //   return `<span for="img" aria-label="book">${book}</span>`;
-    // },
+    
   };
   
