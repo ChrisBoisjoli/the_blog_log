b98d80d36e385278d2e9a4e7f06a575aacab08ff
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-26 13:58:38 -0500

    working on dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,6 +3,48 @@ const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 //get Dashboard
+// Create post
+router.get('/create', withAuth, async (req, res) => {
+  try {
+      res.render('create', {
+      loggedIn: req.session.loggedIn,
+      });
+  }
+  catch (err) {
+      res.status(500).json(err);
+  }
+  });
+  // Get all posts by user
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll(
+      {
+      include: [
+        {
+          model: User,
+          attributes: [
+            'username',
+          ],
+        },
+      ],
+        where: {
+          user_id: req.session.user_id,
+        }
+      });
+    
+    const user_posts = postData.map((user_post) => 
+    user_post.get( { plain: true })
+    );
+    res.render('dashboard', {
+      user_posts,
+      loggedIn: req.session.loggedIn,
+    });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+  
 router.get('/dashboard/', withAuth, async (req, res) => {
     try {
       const dbPostData = await Post.findByPk(req.params.id, {
@@ -26,6 +68,22 @@ router.get('/dashboard/', withAuth, async (req, res) => {
       res.status(500).json(err);
     }
   });
+  // Create new post with user input.
+router.post('/create', async (req, res) => {
+  try {
+      const userData = await Post.create(
+      {
+          title: req.body.title,
+          content: req.body.content,
+          user_id: req.session.user_id,
+      });
+      res.status(200).json(userData);
+  }
+  catch (err) {
+      res.status(400).json(err);
+  }
+  });
+  
   
 
 
